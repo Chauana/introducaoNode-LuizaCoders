8d1f1aa67701bacc2353658ea5a792bfcb1aa4d6
Chauana Januario <chauana.oliveira@magazineluiza.com.br>
2021-09-01 22:11:28 -0300

    Primeiro Commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default{
+    secret:'a057e36391f93e4c9760ab895379aac1',
+    expiresIn:'1d',
+}
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'portgress',
+    host: 'chunee.db.elephantsql.com',
+    username:'stbbwqce',
+    password:'oHLun5-Ip-tzPSSSDfvJ0jCohZ70J593',
+    database:'stbbwqce',
+    define:{
+        timesStamps: true,
+        underscored: true, 
+        underscoredAll: true,
+    
+    }
+}
